8c7faf9f66cd837f73b0d0321a9a32612171023d
Andrew Cutler <macropin@gmail.com>
2014-10-17 21:49:15 +1100

    add note about importlib

--- webhooks/settings.py
+++ webhooks/settings.py
@@ -10,7 +10,7 @@ WEB_HOOK_OWNER_MODEL = getattr(settings, 'WEB_HOOK_OWNER_MODEL', WEB_HOOK_OWNER_
 WEB_HOOK_ACTIONS = getattr(settings, 'WEB_HOOK_ACTIONS', WEB_HOOK_ACTIONS)
 WEB_HOOK_OWNER_LOCAL = getattr(settings, 'WEB_HOOK_OWNER_LOCAL', WEB_HOOK_OWNER_LOCAL)
 
-# Work out our dynamic relation. Nb. Django 1.7 has a function to do this.
+# Work out our dynamic relation. Nb. Django 1.7 has a function to do this. (django.utils.importlib import_module)
 app_name = WEB_HOOK_OWNER_MODEL.rsplit('.', 1)[0]
 model_name = WEB_HOOK_OWNER_MODEL.rsplit('.', 1)[1]
 module = __import__(app_name, fromlist=[model_name])
